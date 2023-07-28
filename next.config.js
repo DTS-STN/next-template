e3e2db67bd09fd78a9ca48e1ecf1171e4994de8b
frank basham <frankabasham@gmail.com>
2023-07-28 08:23:06 -0400

    [frontend] clean styles, security headers, font imports
    [frontend] update csp, styles, and font imports

--- next.config.js
+++ next.config.js
@@ -11,37 +11,25 @@ const builddate = process.env.BUILD_DATE
 
 const securityHeaders = [
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
     key: 'X-Frame-Options',
     value: 'DENY',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value:
+      "default-src 'self'; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'; script-src-elem 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:",
   },
   {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
   {
+    key: 'Permissions-Policy',
+    value: 'camera=(); battery=(); geolocation=(); microphone=()',
+  },
+  {
     key: 'Referrer-Policy',
     value: 'origin-when-cross-origin',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '0',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value:
-      "default-src 'self'; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'; script-src-elem 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:",
   },
 ]
 
@@ -56,7 +44,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: '/(.*)',
         headers: securityHeaders,
       },
     ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        display: ['Lato', 'sans-serif'],
-        body: ['Noto sans', 'sans-serif'],
-        extra: ['Patua One', 'cursive'],
-      },
-      colors: {
-        current: 'currentColor',
-      },
       backgroundImage: () => ({
         'footer-parliament-image': 'url(../../public/assets/landscape.png)',
       }),
-      boxShadow: {
-        card: '0px 2px 8px rgba(0, 0, 0, 0.25)',
-      },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
 }
